c3c0945c3e643869c723075c10527417132330da
Hiroshi Okazaki <saitou11yuichi24mail@yahoo.co.jp>
2024-03-29 07:03:26 +0900

    勤怠の登録と表示の最低限の機能まで完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,95 @@
-import Image from "next/image";
+'use client';
+
 import styles from "./page.module.css";
+import { Key, useEffect, useState } from "react";
+
+const STORAGE_KEY = 'timecard';
+
+type Timecard = {
+  goToWork: string[],
+  leaveWork: string[],
+};
+
+type Timecards = {
+  [Key: string]: Timecard,
+}
 
 export default function Home() {
+  const [timecard, setTimecard] = useState<Timecards>({});
+  useEffect(() => {
+    setTimecard(JSON.parse(localStorage.getItem(STORAGE_KEY) ?? '{}'));
+  }, []);
+  useEffect(() => {
+    if (Object.keys(timecard).length) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(timecard));
+    }
+  }, [timecard]);
+
+  const goToWork = () => {
+    const tmpTimecard = { ...timecard };
+    const today = new Date();
+    const todayKey = `${today.getFullYear()}/${('0' + (today.getMonth() + 1)).slice(-2)}/${('0' + today.getDate()).slice(-2)}`;
+    const timeStr = `${('0' + today.getHours()).slice(-2)}:${('0' + today.getMinutes()).slice(-2)}`;
+
+    tmpTimecard[todayKey] = tmpTimecard[todayKey] ?? {};
+    tmpTimecard[todayKey]['goToWork'] = tmpTimecard[todayKey]['goToWork'] ?? [];
+    tmpTimecard[todayKey]['goToWork'].push(timeStr);
+
+    setTimecard(tmpTimecard);
+  };
+
+  const leaveWork = () => {
+    const tmpTimecard = { ...timecard };
+    const today = new Date();
+    const todayKey = `${today.getFullYear()}/${('0' + (today.getMonth() + 1)).slice(-2)}/${('0' + today.getDate()).slice(-2)}`;
+    const timeStr = `${('0' + today.getHours()).slice(-2)}:${('0' + today.getMinutes()).slice(-2)}`;
+
+    tmpTimecard[todayKey] = tmpTimecard[todayKey] ?? {};
+    tmpTimecard[todayKey]['leaveWork'] = tmpTimecard[todayKey]['leaveWork'] ?? [];
+    tmpTimecard[todayKey]['leaveWork'].push(timeStr);
+
+    setTimecard(tmpTimecard);
+  };
+
   return (
     <main className={styles.main}>
+      <div className={styles.title}>
+        シンプルタイムカード
+      </div>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <ul>
+          <li>ブラウザ上に保存するタイプのオフラインのタイムカードです。</li>
+          <li>タイムカードの情報はどこにも送信されないため安心してお使いください。</li>
+          <li>保存した時刻を消す機能は今後開発予定です。</li>
+        </ul>
       </div>
+      <div>
+        <button className={styles.button} onClick={goToWork}>
+          出勤
+        </button>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <button className={styles.button} onClick={leaveWork}>
+          退勤
+        </button>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <table className={styles.timecard_table}>
+        <thead>
+          <tr>
+            <td>日付</td>
+            <td>出勤時間</td>
+            <td>退勤時間</td>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(timecard).length ? Object.keys(timecard).map(d => {
+            return <tr key={d}>
+              <td key={'date_' + d}>{d}</td>
+              <td key={'goToWork_' + d}>{(timecard[d]['goToWork'] ?? []).join('\n')}</td>
+              <td key={'leaveWork_' + d}>{(timecard[d]['leaveWork'] ?? []).join('\n')}</td>
+            </tr>
+          }) : <tr><td colSpan={3}>勤怠情報なし</td></tr>}
+        </tbody>
+      </table>
     </main>
   );
 }
