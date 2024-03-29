51ecd2f088807612db79f20644c843741f958d6a
Hiroshi Okazaki <saitou11yuichi24mail@yahoo.co.jp>
2024-03-29 19:48:39 +0900

    出力に合わせて設定修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/simple_timecard_app',
     output: 'export',
+    reactStrictMode: true,
 };
 
 export default nextConfig;
